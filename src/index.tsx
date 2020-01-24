a8bbb52f3a1e2cf69ffc6f0526a19a403066d395
Vincent Audoire <vincent8992@gmail.com>
2020-01-24 18:45:54 +0100

    Fix generating ts files

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import DateSelector from "./lib/components/dateSelector";
+
 ReactDOM.render(<App />, document.getElementById("root"));
+
+export { DateSelector };
