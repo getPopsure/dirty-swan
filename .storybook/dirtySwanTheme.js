d48745689154c31de9bf076c34c33082a54208be
Vincent Audoire <vincent8992@gmail.com>
2020-01-26 13:06:58 +0100

    theming dirtyswan

---
+++ .storybook/dirtySwanTheme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "Dirty swan",
+  brandUrl: "https://dirtyswan.design",
+  brandImage: ""
+});
