0027ca29872a8d880868136f4a3d0fed8f4f166e
Vincent Audoire <vincent8992@gmail.com>
2020-03-24 16:16:33 +0100

    v0.7.0--alpha-4

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import DateSelector from "./lib/components/dateSelector";
-import Dropzone from "./lib/components/dropZone";
+import Dropzone from "./lib/components/dropzone";
 
 export { DateSelector, Dropzone };
 
--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,4 @@
 import DateSelector from "./components/dateSelector";
+import Dropzone from "./components/dropzone";
 
-export { DateSelector };
+export { DateSelector, Dropzone };
