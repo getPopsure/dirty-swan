db2afaca1ee2217de1b99737c15808031848d93a
Vincent Audoire <vincent8992@gmail.com>
2020-04-03 11:28:52 +0200

    v0.7.6

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import DateSelector from "./lib/components/dateSelector";
-import Dropzone from "./lib/components/dropzone";
-import SignaturePad from "./lib/components/signaturePad";
-
-export { DateSelector, Dropzone, SignaturePad };
-
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,4 +1,5 @@
 import DateSelector from "./components/dateSelector";
 import Dropzone from "./components/dropzone";
+import SignaturePad from "./components/signaturePad";
 
-export { DateSelector, Dropzone };
+export { DateSelector, Dropzone, SignaturePad };
