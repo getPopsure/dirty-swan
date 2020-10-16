73500cfd2546aeaa3e702f6b77964066c6b1abcb
Vincent Audoire <vincent8992@gmail.com>
2020-10-16 12:02:20 +0200

    Add JSX input component

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,5 +2,6 @@ import DateSelector from "./components/dateSelector";
 import Dropzone from "./components/dropzone";
 import SignaturePad from "./components/signaturePad";
 import AutocompleteAddress from "./components/autocompleteAddress";
+import Input from "./components/input";
 
-export { DateSelector, Dropzone, SignaturePad, AutocompleteAddress };
+export { DateSelector, Dropzone, SignaturePad, AutocompleteAddress, Input };
