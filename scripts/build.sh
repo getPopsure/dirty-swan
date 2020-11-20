f6adc8e0cc947e47e6c13255c90ba66925257f48
Vincent Audoire <vincent8992@gmail.com>
2020-11-20 10:31:10 +0100

    v0.14.0-alpha.16

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 
 rm -rf dist
 mkdir -p dist/tmp 
+mkdir -p dist/colors 
 cp -a -v src/lib/scss/private/components/assets dist
 cp -a -v src/lib/scss dist/tmp
 cp -v src/lib/scss/public/*.scss dist
+cp -v src/lib/scss/public/colors/*.scss dist/colors
 rollup -c
