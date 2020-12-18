5a82525e7186fb88e11225b337029e7a436a9ffa
Vincent Audoire <vincent8992@gmail.com>
2020-12-18 10:29:59 +0100

    [NoTicket] Theming storybook

--- .storybook/dirtySwanTheme.js
+++ .storybook/dirtySwanTheme.js
@@ -5,4 +5,13 @@ export default create({
   brandTitle: 'Dirty swan',
   brandUrl: 'https://dirtyswan.design',
   brandImage: '',
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+  textColor: 'black',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+  barTextColor: '#b1b0f5',
+  barSelectedColor: '#8e8cee',
+
+  appBorderColor: '#d2d2d8',
+  appBorderRadius: 8,
 });
