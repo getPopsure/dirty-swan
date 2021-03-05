a283a38b89114137fbf0feb0ac75605c098900b9
Vincent Audoire <vincent8992@gmail.com>
2021-03-05 21:01:58 +0100

    v0.15.9-alpha.2

---
+++ src/lib/components/cards/cardWithTopLeftIcon/index.tsx
@@ -0,0 +1,53 @@
+import {
+  associatedClassForCardState,
+  CardProps,
+  headingForTitleSize,
+} from '..';
+import { Icon, arrowRight, featherLogo } from '../icons';
+
+import styles from './style.module.scss';
+
+export default ({
+  className,
+  title,
+  titleSize = 'medium',
+  children,
+  leftIcon,
+  rightIcon,
+  state = 'actionable',
+  ...props
+}: CardProps & {
+  titleSize?: 'medium' | 'big';
+  leftIcon?: 'logo' | Icon;
+  rightIcon?: 'arrow' | Icon;
+}) => (
+  <div
+    className={`${associatedClassForCardState(state)} ${styles.container} ${
+      className ?? ''
+    }`}
+    {...props}
+  >
+    <div className={styles['title-container']}>
+      {leftIcon && (
+        <img
+          className="mr8"
+          width="28px"
+          height="28px"
+          src={leftIcon === 'logo' ? featherLogo.src : leftIcon.src}
+          alt={leftIcon === 'logo' ? featherLogo.alt : leftIcon.src}
+        />
+      )}
+      <div className={headingForTitleSize(titleSize)}>{title}</div>
+      {rightIcon && (
+        <img
+          className={styles['right-icon']}
+          width="24px"
+          height="24px"
+          src={rightIcon === 'arrow' ? arrowRight.src : rightIcon.src}
+          alt={rightIcon === 'arrow' ? arrowRight.alt : rightIcon.alt}
+        />
+      )}
+    </div>
+    <div className="mt16">{children}</div>
+  </div>
+);
