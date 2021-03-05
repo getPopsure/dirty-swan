b2ec55a484c81b9848af8bd667934f062a17faf3
Vincent Audoire <vincent8992@gmail.com>
2021-03-05 20:44:32 +0100

    Adding card component

---
+++ src/lib/components/card/icons/index.ts
@@ -0,0 +1,25 @@
+import arrowRightImage from './arrow-right.svg';
+import featherLogoImage from './feather-logo.svg';
+import infoImage from './info.svg';
+
+export interface Icon {
+  src: string;
+  alt: string;
+}
+
+const arrowRight: Icon = {
+  src: arrowRightImage,
+  alt: 'arrow pointing right',
+};
+
+const featherLogo: Icon = {
+  src: featherLogoImage,
+  alt: 'feather logo',
+};
+
+const info: Icon = {
+  src: infoImage,
+  alt: 'circle with the letter i inside for information',
+};
+
+export { arrowRight, featherLogo, info };
---
+++ src/lib/components/card/infoCard/index.tsx
@@ -0,0 +1,44 @@
+import { associatedClassForCardState, CardProps } from '..';
+import { Icon, info } from '../icons';
+
+import styles from './style.module.scss';
+
+export default ({
+  className,
+  title,
+  children,
+  topIcon,
+  rightIcon,
+  state = 'actionable',
+  ...props
+}: CardProps & {
+  topIcon: Icon;
+  rightIcon?: 'info' | Icon;
+}) => (
+  <div className={`${styles['root-container']} ${className ?? ''}`} {...props}>
+    <div className={styles['top-icon-background']}>
+      <img
+        src={topIcon.src}
+        alt={topIcon.alt}
+        className={styles['top-icon']}
+        width="48px"
+        height="48px"
+      />
+    </div>
+    <div
+      className={`${associatedClassForCardState(state)} ${styles.container}`}
+    >
+      {rightIcon && (
+        <img
+          width="20px"
+          height="20px"
+          className={styles['right-icon']}
+          src={rightIcon === 'info' ? info.src : rightIcon.src}
+          alt={rightIcon === 'info' ? info.alt : rightIcon.alt}
+        />
+      )}
+      <div className="p-h4 ta-center mt64">{title}</div>
+      {children}
+    </div>
+  </div>
+);
