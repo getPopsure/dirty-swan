68182b1d324a8d5aa345ac03fb92f975af925615
Seungha <kseha1080@gmail.com>
2021-08-25 16:42:36 +0200

    [CHA-2683] Extend Input component to have error styles and add error message

--- src/lib/components/input/index.tsx
+++ src/lib/components/input/index.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import styles from './style.module.scss';
 
 // Something weird is going on with enterKeyHint that makes it a required field under certain circumstances. The & Omit<…> and & Pick<…> is a hacky way to go around that.
-export type InputProps = { hasError?: boolean; prefix?: string } & Omit<
-  JSX.IntrinsicElements['input'],
-  'enterKeyHint'
-> &
+export type InputProps = {
+  hasError?: boolean;
+  prefix?: string;
+  errorMessage?: string;
+} & Omit<JSX.IntrinsicElements['input'], 'enterKeyHint'> &
   Partial<Pick<JSX.IntrinsicElements['input'], 'enterKeyHint'>>;
 
 export default React.forwardRef(
@@ -27,14 +28,27 @@ export default React.forwardRef(
         placeholder=" "
         {...props}
       />
-      {prefix && <span className={styles.prefix}>{prefix}</span>}
+      {prefix && (
+        <span
+          className={`${styles.prefix} ${
+            props.hasError ? styles['prefix--with-error'] : ''
+          }`}
+        >
+          {prefix}
+        </span>
+      )}
       <span
         className={`${styles.placeholder} ${
           prefix ? styles['placeholder--with-prefix'] : ''
-        }`}
+        } ${props.hasError ? styles['placeholder--with-error'] : ''}`}
       >
         {placeholder}
       </span>
+      {props.hasError && props.errorMessage && (
+        <p className={`p-p--small tc-red-500 w100 ${styles.error}`}>
+          {props.errorMessage}
+        </p>
+      )}
     </div>
   )
 );
