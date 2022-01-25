e11e62a4cd73a8f449a9e32f6b1ae9348bf017dc
Kim Kwanka <kimkwanka@gmail.com>
2022-01-25 11:32:30 +0100

    refactor(Cards): move style defs out of the JSX to make cards easier to reason about
    This moves all style string definitions out of the JSX right to beginning of the card components to have them all in one place for future adjustments.

--- src/lib/components/cards/cardWithTopIcon/index.tsx
+++ src/lib/components/cards/cardWithTopIcon/index.tsx
@@ -23,33 +23,38 @@ export default ({
   topIconSize: IconSize;
   titleSize?: 'small' | 'medium' | 'big';
   rightIcon?: 'arrow' | Icon;
-}) => (
-  <>
-    <div
-      className={`d-flex ${associatedClassForCardState(state, dropshadow)} ${
-        styles.container
-      } ${className ?? ''}`}
-      {...props}
-    >
-      <img
-        width={topIconSize.width}
-        height={topIconSize.height}
-        alt={topIcon.alt}
-        src={topIcon.src}
-      />
-      <div className="d-flex mt16">
-        <div className={headingForTitleSize(titleSize)}>{title}</div>
-        {rightIcon && (
-          <img
-            className={styles['right-icon']}
-            width="24px"
-            height="24px"
-            src={rightIcon === 'arrow' ? arrowRight.src : rightIcon.src}
-            alt={rightIcon === 'arrow' ? arrowRight.alt : rightIcon.alt}
-          />
-        )}
+}) => {
+  const cardStyle = `d-flex fd-column ai-center ${className} ${associatedClassForCardState(
+    state,
+    dropshadow
+  )} ${styles.container}`;
+
+  const headingStyle = headingForTitleSize(titleSize);
+  const iconStyle = styles['right-icon'];
+
+  return (
+    <>
+      <div className={cardStyle} {...props}>
+        <img
+          width={topIconSize.width}
+          height={topIconSize.height}
+          alt={topIcon.alt}
+          src={topIcon.src}
+        />
+        <div className="d-flex mt16">
+          <div className={headingStyle}>{title}</div>
+          {rightIcon && (
+            <img
+              className={iconStyle}
+              width="24px"
+              height="24px"
+              src={rightIcon === 'arrow' ? arrowRight.src : rightIcon.src}
+              alt={rightIcon === 'arrow' ? arrowRight.alt : rightIcon.alt}
+            />
+          )}
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
-  </>
-);
+    </>
+  );
+};
