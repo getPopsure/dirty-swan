6d3c3ebb4a599db8a43d9746cd8bf9ac6cb922bb
Kim Kwanka <kimkwanka@gmail.com>
2022-01-25 09:58:49 +0100

    feat(CardWithLeftIcon): [STO-3386] implement missing xsmall variant
    This implements the missing "xsmall" variant of the CardWithLeftIcon component to match the corresponding checkbox component.

--- src/lib/components/cards/cardWithLeftIcon/index.tsx
+++ src/lib/components/cards/cardWithLeftIcon/index.tsx
@@ -7,8 +7,21 @@ import { Icon, arrowRight } from '../icons';
 
 import styles from './style.module.scss';
 
+const containerStyleFromTitleSize = (
+  titleSize: 'xsmall' | 'small' | 'medium' | 'big'
+): string => {
+  switch (titleSize) {
+    case 'xsmall':
+      return 'container--xsmall';
+    case 'small':
+      return 'container--small';
+    default:
+      return 'container';
+  }
+};
+
 export default ({
-  className,
+  className = '',
   title,
   titleSize = 'medium',
   children,
@@ -18,41 +31,44 @@ export default ({
   dropshadow = true,
   ...props
 }: CardProps & {
-  titleSize?: 'small' | 'medium' | 'big';
+  titleSize?: 'xsmall' | 'small' | 'medium' | 'big';
   leftIcon?: Icon;
   rightIcon?: 'arrow' | Icon;
-}) => (
-  <div
-    className={`${associatedClassForCardState(state, dropshadow)} d-flex ${
-      styles.container
-    } ${className ?? ''} ${
-      titleSize === 'small' ? styles['container--small'] : ''
-    }`}
-    {...props}
-  >
-    {leftIcon && (
-      <img
-        width="48px"
-        height="48px"
-        className={`mr32 ${styles['left-icon']}`}
-        src={leftIcon.src}
-        alt={leftIcon.alt}
-      />
-    )}
-    <div>
-      <div className="d-flex">
-        <div className={headingForTitleSize(titleSize)}>{title}</div>
-        {rightIcon && (
-          <img
-            className={styles['right-icon']}
-            width="24px"
-            height="24px"
-            src={rightIcon === 'arrow' ? arrowRight.src : rightIcon.src}
-            alt={rightIcon === 'arrow' ? arrowRight.alt : rightIcon.alt}
-          />
-        )}
+}) => {
+  const cardStyle = `d-flex ai-center ${className} ${associatedClassForCardState(
+    state,
+    dropshadow
+  )} ${styles[containerStyleFromTitleSize(titleSize)]}`;
+
+  const headingStyle = headingForTitleSize(titleSize);
+  const iconStyle = titleSize === 'xsmall' ? 'mr16' : 'mr32';
+
+  return (
+    <div className={cardStyle} {...props}>
+      {leftIcon && (
+        <img
+          width="48px"
+          height="48px"
+          className={iconStyle}
+          src={leftIcon.src}
+          alt={leftIcon.alt}
+        />
+      )}
+      <div>
+        <div className="d-flex">
+          <div className={headingStyle}>{title}</div>
+          {rightIcon && (
+            <img
+              className="ml-auto"
+              width="24px"
+              height="24px"
+              src={rightIcon === 'arrow' ? arrowRight.src : rightIcon.src}
+              alt={rightIcon === 'arrow' ? arrowRight.alt : rightIcon.alt}
+            />
+          )}
+        </div>
+        <p className="p-p mt8 tc-grey-600">{children}</p>
       </div>
-      <p className="p-p mt8 tc-grey-600">{children}</p>
     </div>
-  </div>
-);
+  );
+};
