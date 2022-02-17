4673c7c78e374f7ebedc6dd073fc3ca59c3fe19a
Kim Kwanka <kimkwanka@gmail.com>
2022-02-17 11:31:36 +0100

    feat(Colors): [EMU-3942] Add glacier, spearmint and pink color families

--- src/lib/scss/public/demo.tsx
+++ src/lib/scss/public/demo.tsx
@@ -2,31 +2,6 @@ import React from 'react';
 
 const colors = [
   {
-    name: 'Blue 100',
-    code: 'blue-100',
-    hex: '#e5f0ff',
-  },
-  {
-    name: 'Blue 300',
-    code: 'blue-300',
-    hex: '#b0cdf3',
-  },
-  {
-    name: 'Blue 500',
-    code: 'blue-500',
-    hex: '#8bb4ea',
-  },
-  {
-    name: 'Blue 700',
-    code: 'blue-700',
-    hex: '#5f7ba0',
-  },
-  {
-    name: 'Blue 900',
-    code: 'blue-900',
-    hex: '#2d394a',
-  },
-  {
     name: 'Primary 25',
     code: 'primary-25',
     hex: '#fcfcff',
@@ -62,6 +37,56 @@ const colors = [
     hex: '#2e2e4c',
   },
   {
+    name: 'Blue 100',
+    code: 'blue-100',
+    hex: '#e5f0ff',
+  },
+  {
+    name: 'Blue 300',
+    code: 'blue-300',
+    hex: '#b0cdf3',
+  },
+  {
+    name: 'Blue 500',
+    code: 'blue-500',
+    hex: '#8bb4ea',
+  },
+  {
+    name: 'Blue 700',
+    code: 'blue-700',
+    hex: '#5f7ba0',
+  },
+  {
+    name: 'Blue 900',
+    code: 'blue-900',
+    hex: '#2d394a',
+  },
+  {
+    name: 'Glacier 100',
+    code: 'glacier-100',
+    hex: '#e0f7fe',
+  },
+  {
+    name: 'Glacier 300',
+    code: 'glacier-300',
+    hex: '#aeddec',
+  },
+  {
+    name: 'Glacier 500',
+    code: 'glacier-500',
+    hex: '#8bcbdf',
+  },
+  {
+    name: 'Glacier 700',
+    code: 'glacier-700',
+    hex: '#5d8896',
+  },
+  {
+    name: 'Glacier 900',
+    code: 'glacier-900',
+    hex: '#2d4148',
+  },
+  {
     name: 'Red 100',
     code: 'red-100',
     hex: '#fedede',
@@ -85,6 +110,31 @@ const colors = [
     name: 'Red 900',
     code: 'red-900',
     hex: '#4b2525',
+  },
+  {
+    name: 'Pink 100',
+    code: 'pink-100',
+    hex: '#ffebf1',
+  },
+  {
+    name: 'Pink 300',
+    code: 'pink-300',
+    hex: '#ffb1cb',
+  },
+  {
+    name: 'Pink 500',
+    code: 'pink-500',
+    hex: '#f96092',
+  },
+  {
+    name: 'Pink 700',
+    code: 'pink-700',
+    hex: '#c0305f',
+  },
+  {
+    name: 'Pink 900',
+    code: 'pink-900',
+    hex: '#700024',
   },
   {
     name: 'Grey 100',
@@ -145,6 +195,31 @@ const colors = [
     name: 'Green 900',
     code: 'green-900',
     hex: '#354a2d',
+  },
+  {
+    name: 'Spearmint 100',
+    code: 'spearmint-100',
+    hex: '#e3fff2',
+  },
+  {
+    name: 'Spearmint 300',
+    code: 'spearmint-300',
+    hex: '#aaeacc',
+  },
+  {
+    name: 'Spearmint 500',
+    code: 'spearmint-500',
+    hex: '#85dcb4',
+  },
+  {
+    name: 'Spearmint 700',
+    code: 'spearmint-700',
+    hex: '#599278',
+  },
+  {
+    name: 'Spearmint 900',
+    code: 'spearmint-900',
+    hex: '#2b4639',
   },
   {
     name: 'Yellow 100',
