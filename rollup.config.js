2ad0d650c9e0079f0003b6d7d9264d8581ec4238
busypeoples <busypeoples@gmail.com>
2023-01-25 11:05:12 +0100

    fix(treeshaking): [NoTicket] initial proof of concept

--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,75 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
+
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const postcssCustomProperties = require('postcss-custom-properties');
 
-export default {
-  input: 'src/lib/index.tsx',
-  output: [
-    {
-      dir: 'dist',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true,
-      plugins: [postcssCustomProperties({ preserve: true })],
-    }),
-    url(),
-    resolve(),
-    typescript(),
-    commonjs(),
-  ],
-};
+/**
+ *
+ * We need to decide how to isolate cjs and esm builds
+ * For now this totally separates the two builds by defining
+ * explicit input/output properties.
+ *
+ * Need to look into which plugins are needed for each build.
+ *
+ */
+export default [
+  {
+    input: 'src/lib/index.tsx',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      postcss({
+        modules: true,
+        plugins: [postcssCustomProperties({ preserve: true })],
+      }),
+      url(),
+      resolve(),
+      typescript(),
+      commonjs(),
+    ],
+  },
+  {
+    input: Object.fromEntries(
+      glob
+        // we need to look into why adding ts breaks the build
+        .sync('src/**/*(*.tsx)')
+        .map((file) => [
+          path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+    ),
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      postcss({
+        modules: true,
+        plugins: [postcssCustomProperties({ preserve: true })],
+      }),
+      url(),
+      resolve(),
+      typescript(),
+      commonjs(),
+    ],
+  },
+];
