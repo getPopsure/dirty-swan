1dc921d596f966d0bfc5de07374483cab3edb330
Diogo Mateus <diogo@feather-insurance.com>
2023-02-17 11:32:22 +0000

    fix(generateId): Add additional check for undefined window (#185)

--- src/lib/util/generateId/index.ts
+++ src/lib/util/generateId/index.ts
@@ -1,5 +1,5 @@
 function generateId(): string {
-  if (window?.crypto?.getRandomValues) {
+  if (typeof window !== "undefined" && window?.crypto?.getRandomValues) {
     return (
       `${[1e7]}-${1e3}-${4e3}-${8e3}-${1e11}`).replace(/[018]/g,
       (c: any) =>
