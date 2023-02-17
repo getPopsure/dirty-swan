96aa8a38ea9f89984f90c238d2a0b683819f83cd
Diogo Mateus <diogo@feather-insurance.com>
2023-02-17 11:06:19 +0000

    refactor(uuid): [noticket]: Replace uuid package with generateId util (#184)
    * refactor(uuid): [noticket]: Replace uuid package with generateId util
    
    * Default generate Id to use crypto.randomUUID
    
    * Replace randomUUID with random values

---
+++ src/lib/util/generateId/index.ts
@@ -0,0 +1,15 @@
+function generateId(): string {
+  if (window?.crypto?.getRandomValues) {
+    return (
+      `${[1e7]}-${1e3}-${4e3}-${8e3}-${1e11}`).replace(/[018]/g,
+      (c: any) =>
+        (((c ^ window.crypto.getRandomValues(new Uint8Array(1))[0]) & 15) >> c / 4).toString(16)
+    );
+  }
+
+  return 'xxxx-xxxx-xxx-xxxx'.replace(/[x]/g, () => (
+    Math.floor(Math.random() * 16).toString(16)
+  ));
+}
+
+export default generateId;
