08b848c4d154682462103537fee0b1ef40acb7e3
busypeoples <busypeoples@gmail.com>
2023-03-31 09:36:59 +0200

    Add dateSelector calendar tests

---
+++ src/lib/components/dateSelector/index.test.tsx
@@ -0,0 +1,53 @@
+import { render, fireEvent } from '../../util/testUtils';
+
+import DateSelector from '.';
+
+const setup = (date: string, onChange: (date: string) => void = () => {}) => {
+  return render(
+    <DateSelector
+      value={date}
+      yearBoundaries={{ min: 2020, max: 2025 }}
+      onChange={onChange}
+      displayCalendar
+    />
+  );
+};
+
+describe('DateSelector component', () => {
+  it('should return the selected date when clicking on the calendar', async () => {
+    const callback = jest.fn();
+    const date = '2024-01-01';
+    const expectedDate = '2024-01-17';
+    const { getByAltText, getByLabelText, user } = setup(date, callback);
+    const button = getByAltText('calendar');
+
+    await user.click(button);
+
+    const calendarCell = getByLabelText(/17 2024/);
+
+    await user.click(calendarCell);
+
+    expect(callback).toHaveBeenCalledWith(expectedDate);
+
+    expect(calendarCell).not.toBeVisible();
+  });
+
+  it('should close the calendar when clicking outside', async () => {
+    const callback = jest.fn();
+    const date = '2024-01-01';
+    const { getByAltText, getByLabelText, user } = setup(date, callback);
+    const button = getByAltText('calendar');
+
+    await user.click(button);
+
+    const calendarCell = getByLabelText(/17 2024/);
+    expect(calendarCell).toBeVisible();
+
+    // click outside the calendar
+    await user.click(document.body);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    expect(calendarCell).not.toBeVisible();
+  });
+});
