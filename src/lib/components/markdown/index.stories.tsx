2f723868e161981ef7cf89dad1175e6af5d03f76
Diogo Mateus <diogo@feather-insurance.com>
2023-06-09 12:22:03 +0100

    fix(markdown): Remove .md definitions

--- src/lib/components/markdown/index.stories.tsx
+++ src/lib/components/markdown/index.stories.tsx
@@ -1,5 +1,4 @@
 import { Markdown, MarkdownProps } from '.';
-import example from './example.md';
 
 const story = {
   title: 'JSX/Markdown',
@@ -52,7 +51,70 @@ MarkdownStory.storyName = "Markdown";
 
 export const AvailableMDComponents = ({ className }: MarkdownProps) => (
   <Markdown className={className}>
-    {example}
+    {`# Headers
+The header options range from h1 to h4.
+
+\`\`\`
+# H1
+## H2
+### H3
+#### H4
+\`\`\`
+
+# H1 Lorem ipsum non sunt qui sunt
+
+## H2 Lorem ipsum non sunt qui sunt
+
+### H3 Lorem ipsum non sunt qui sunt
+
+#### H4 Lorem ipsum non sunt qui sunt
+
+# Emphasis
+
+\`\`\`
+Emphasis or italics, use *asterisk* or _underscore_.
+Strong emphasis or bold, use **asterisks** or __underscores__.
+Combined emphasis, use **_asterisks and underscores_**.
+\`\`\`
+
+Emphasis or italics, use _asterisk_ or _underscore_.
+
+Strong emphasis or bold, use **asterisks** or **underscores**.
+
+Combined emphasis, use **_asterisks and underscores_**.
+
+# Lists
+
+\`\`\`
+1. First ordered list item
+2. Another item
+3. Actual numbers don't matter, just that it's a number
+4. And another item.
+
+* Unordered list can use asterisks
+- Or minuses
++ Or pluses
+\`\`\`
+
+1. First ordered list item
+2. Another item
+3. Actual numbers don't matter, just that it's a number
+4. And another item.
+
+- Unordered list can use asterisks
+- Or minuses
+- Or pluses
+
+# Links
+
+For links markdown syntax should be used.
+
+\`\`\`
+[This is an inline link](https://feather-insurance.com)
+\`\`\`
+
+[This is an inline link](https://feather-insurance.com)
+`}
   </Markdown>
 );
 
