6eada25e53cb5896f0effbf8eabdad6d835b06b2
busypeoples <busypeoples@gmail.com>
2023-06-20 15:42:28 +0200

    chore(icons): Add more icons

--- src/lib/util/images/index.ts
+++ src/lib/util/images/index.ts
@@ -8,12 +8,19 @@ const images = {
     dogLiability: `${basePath}/dogLiabilityIcon.png`,
     dogLiability2x: `${basePath}/dogLiabilityIcon2x.png`,
     dogLiability3x: `${basePath}/dogLiabilityIcon3x.png`,
+    lifeIcon: `${basePath}/lifeIcon.png`,
+    lifeIcon2x: `${basePath}/lifeIcon2x.png`,
+    lifeIcon3x: `${basePath}/lifeIcon3x.png`,
   },
   brokenAquarium: `${basePath}/brokenAquarium.svg`,
   brokenGlass: `${basePath}/brokenGlass.svg`,
   damagedLaptop: `${basePath}/damagedLaptop.svg`,
   moneyIncome: `${basePath}/moneyIncome.svg`,
   washingMachine: `${basePath}/washingMachine.svg`,
+  bill: `${basePath}/bill.svg`,
+  books: `${basePath}/books.svg`,
+  finalExpenses: `${basePath}/finalExpenses.svg`,
+  mortgage: `${basePath}/mortgage.svg`,
 } as const;
 
 export { images };
