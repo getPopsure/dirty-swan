df5922c22afdc2f7b40e14d558156889c4e58d4c
ruheni <alex.ruheni@feather-insurance.com>
2024-02-20 14:06:14 +0100

    refactor(IbanInput): Make IbanInput onChange prop optional

--- src/lib/components/input/iban/index.tsx
+++ src/lib/components/input/iban/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import type { SyntheticEvent } from 'react';
 import { Input, InputProps } from '..';
 
 import { formatIban } from './formatIban';
@@ -8,13 +10,20 @@ const IbanInput = ({
   ...props
 }: {
   value?: string;
-  onChange: (value: string) => void;
-} & Omit<InputProps, 'onChange' | 'value' | 'ref'>) => (
-  <Input
-    value={formatIban(value)}
-    onChange={(e) => onChange(formatIban(e.target.value))}
+  onChange?: (value: string) => void;
+} & Omit<InputProps, 'onChange' | 'value' | 'ref'>) => {
+  const [iban, setIban] = useState<string | undefined>(value);
+
+  const handleChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    setIban(e.currentTarget.value);
+    onChange?.(formatIban(e.currentTarget.value));
+  }
+  return <Input
+    name='iban'
+    value={formatIban(iban)}
+    onChange={handleChange}
     {...props}
   />
-);
+};
 
 export default IbanInput;
