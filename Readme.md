2f781d5bdfb2a64f021c8687cd9bc07f0aa9f4af
Diogo Mateus <diogo@feather-insurance.com>
2024-06-19 10:12:05 +0100

    chore(icons): Add ArrowRightFilled icon (#326)

--- Readme.md
+++ Readme.md
@@ -26,3 +26,10 @@ This will start storybook at `http://localhost:9009/`.
 2. Use the "Draft a new release" on GitHub and write down some release notes containing "what's updated", "breaking change" (if any) and "Migration from previous version" (if any)
 
 GitHub Action will then automatically pick it up from there and release the new version for you.
+
+### Adding new icons
+
+1. Add the new SVG file to `src/lib/components/icon/assets/
+2. Run `yarn generate-icons`
+
+This will auto-generate all the typing, exports and typescript/component files.
---
+++ src/lib/components/icon/icons/ArrowRightFilled.tsx
@@ -0,0 +1,23 @@
+/* Generated file. Do not modify. */
+import { createElement } from 'react';
+import { IconWrapper } from '../IconWrapper';
+import type { IconWrapperProps } from '../IconWrapper';
+export default (props: IconWrapperProps) =>
+  createElement(
+    IconWrapper,
+    props,
+    <circle
+      cx={12}
+      cy={12}
+      r={10}
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth={2}
+    />,
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeWidth={2}
+      d="m12 8 3.823 3.823a.25.25 0 0 1 0 .354L12 16m-4-4h8"
+    />
+  );
