36d95a1fe3182464dde98c748cb6a26fd6b78487
Kim Kwanka <kimkwanka@gmail.com>
2024-07-18 13:12:58 +0200

    feat(table): [SIDE-402] Add ButtonCell

---
+++ src/lib/components/table/components/TableCell/ButtonCell/ButtonCell.stories.tsx
@@ -0,0 +1,66 @@
+import { ButtonCell } from './ButtonCell';
+
+const story = {
+  title: 'JSX/Table/Cells',
+  component: ButtonCell,
+  argTypes: {},
+  args: {
+    nFilledBars: 3,
+    isSelected: false,
+    content: 'Premium',
+    subContent: '€277/mo',
+    disabled: false,
+  },
+};
+
+export const ButtonCellStory = ({
+  isSelected,
+  content,
+  subContent,
+  disabled,
+}: React.ComponentProps<typeof ButtonCell>) => (
+  <div className="p48 d-flex fd-column gap16 bg-white">
+    <ButtonCell
+      content={content}
+      subContent={subContent}
+      isSelected={isSelected}
+      disabled={disabled}
+      onClick={() => {}}
+    />
+
+    <ButtonCell
+      content={content}
+      subContent={subContent}
+      isSelected
+      disabled={disabled}
+      onClick={() => {}}
+    />
+
+    <ButtonCell
+      content={content}
+      subContent={subContent}
+      disabled
+      onClick={() => {}}
+    />
+
+    <ButtonCell
+      content={content}
+      isSelected={isSelected}
+      disabled={disabled}
+      onClick={() => {}}
+    />
+
+    <ButtonCell
+      content={content}
+      isSelected
+      disabled={disabled}
+      onClick={() => {}}
+    />
+
+    <ButtonCell content={content} disabled onClick={() => {}} />
+  </div>
+);
+
+ButtonCellStory.storyName = 'ButtonCell';
+
+export default story;
