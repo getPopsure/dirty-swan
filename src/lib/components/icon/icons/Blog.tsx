0c14de70d27b6cdfb57e4cb93434c494254b179c
Alessandra Maffei <113006001+a-maffei@users.noreply.github.com>
2024-08-07 14:44:23 +0200

    feat(icons): [EPIC-419] add BlogIcon to icon library (#376)

---
+++ src/lib/components/icon/icons/Blog.tsx
@@ -0,0 +1,15 @@
+/* Generated file. Do not modify. */
+import { createElement } from 'react';
+import { IconWrapper } from '../IconWrapper';
+import type { IconWrapperProps } from '../IconWrapper';
+export default (props: IconWrapperProps) =>
+  createElement(
+    IconWrapper,
+    props,
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth={2}
+      d="M8 8h8m-8 4h8m-8 4h3m10 4.2V3.8a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v16.4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2Z"
+    />
+  );
