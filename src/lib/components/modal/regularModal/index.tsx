a76558abeb65e7835876751ee17bb59db100d16e
Diogo Mateus <diogo@feather-insurance.com>
2024-08-13 16:05:53 +0100

    refactor(modal): Minor modal style updates (#380)

--- src/lib/components/modal/regularModal/index.tsx
+++ src/lib/components/modal/regularModal/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 import classNames from 'classnames';
 import { GenericModal } from '../genericModal';
 
-const RegularModal = ({ className = '', ...rest }: Props) => (
+const RegularModal = ({ className = '', size, ...rest }: Props) => (
   <GenericModal
     classNames={{
       wrapper: ({ isClosing }) => classNames(
@@ -14,8 +14,9 @@ const RegularModal = ({ className = '', ...rest }: Props) => (
         }
       ),
       container: classNames(
-        'bg-white br8 d-flex ai-center fd-column mx-auto my0 wmx8',
-        styles.container
+        'bg-white br8 d-flex ai-center fd-column mx-auto my0',
+        styles.container,
+        size === 'large' ? 'wmx10' : 'wmx8'
       )
     }}
     {...rest}
