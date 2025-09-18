2ec2e743553bfb9a7506966e6ab7b525d31a2c33
Diogo Mateus <diogo@feather-insurance.com>
2025-09-18 11:54:31 +0100

    refactor(info-card): Add icon and iconWithBackground variants

--- src/lib/components/cards/infoCard/index.tsx
+++ src/lib/components/cards/infoCard/index.tsx
@@ -8,7 +8,7 @@ import classNames from 'classnames';
 export interface InfoCardProps extends CardProps {
   topIcon: ReactNode;
   showIcon?: boolean;
-  topIconType?: 'image' | 'icon' | 'banner';
+  topIconType?: 'image' | 'icon' | 'iconWithBackground' | 'banner';
   disabled?: boolean;
 };
 
@@ -22,63 +22,69 @@ export const InfoCard = ({
   disabled = false,
   onClick,
   ...cardProps
-}: InfoCardProps) => (
-  <Card
-    {...cardProps}
-    label={topIcon && (
-      <>
-        {topIconType === 'icon' ? (
-          <div
-            className={classNames(
-              'd-flex ai-center jc-center bg-orange-200 br-circle p16',
-              styles.topIconWrapper
-            )}
-          >
-            {topIcon}
-          </div>
-        ) : (
-          <div className={classNames(
-            'd-flex ai-center jc-center',
-            { [styles.topIconBanner]: topIconType === 'banner' }
-          )}>
-            {topIcon}
-          </div>
-        )}
-      </>
-    )}
-    title={title && (
-      <>
-        {title}
+}: InfoCardProps) => {
+  const isIconType = topIconType === 'icon' || topIconType === 'iconWithBackground';
 
-        {showIcon && <ArrowRight size={20} />}
-      </>
-    )}
-    titleVariant='medium'
-    description={children}
-    descriptionVariant='small'
-    actionIcon={null}
-    showActionIcon={false}
-    classNames={{
-      buttonWrapper: styles.buttonWrapper,
-      wrapper: classNames({
-        [styles.wrapper]: true,
-        [styles.disabled]: disabled,
-        'pt40': topIcon && topIconType === 'icon',
-        'mt40': topIcon && topIconType === 'icon',
-        [styles.bannerWrapper]: topIcon && topIconType === 'banner',
-      }),
-      label: classNames({
-        [styles.floatingLabel]: topIcon && topIconType !== 'image',
-      }),
-      title: classNames(
-        {'mt16': topIcon && topIconType === 'banner'},
-        'd-flex ai-center jc-center my8'
-      ),
-      description: 'ta-center',
-      contentWrapper: styles.contentWrapper,
-    }}
-    variant='default'
-    onClick={disabled ? undefined : onClick}
-    density='compact'
-  />
-);
+  return (
+    <Card
+      {...cardProps}
+      label={topIcon && (
+        <>
+          {isIconType ? (
+            <div
+              className={classNames(
+                'd-flex ai-center jc-center br-circle p16',
+                styles.topIconWrapper, {
+                  'bg-orange-200': topIconType === 'iconWithBackground'
+                }
+              )}
+            >
+              {topIcon}
+            </div>
+          ) : (
+            <div className={classNames(
+              'd-flex ai-center jc-center',
+              { [styles.topIconBanner]: topIconType === 'banner' }
+            )}>
+              {topIcon}
+            </div>
+          )}
+        </>
+      )}
+      title={title && (
+        <>
+          {title}
+
+          {showIcon && <ArrowRight size={20} />}
+        </>
+      )}
+      titleVariant='medium'
+      description={children}
+      descriptionVariant='small'
+      actionIcon={null}
+      showActionIcon={false}
+      classNames={{
+        buttonWrapper: styles.buttonWrapper,
+        wrapper: classNames({
+          [styles.wrapper]: true,
+          [styles.disabled]: disabled,
+          'pt40': topIcon && isIconType,
+          'mt40': topIcon && isIconType,
+          [styles.bannerWrapper]: topIcon && topIconType === 'banner',
+        }),
+        label: classNames({
+          [styles.floatingLabel]: topIcon && topIconType !== 'image',
+        }),
+        title: classNames(
+          {'mt16': topIcon && topIconType === 'banner'},
+          'd-flex ai-center jc-center my8'
+        ),
+        description: 'ta-center',
+        contentWrapper: styles.contentWrapper,
+      }}
+      variant='default'
+      onClick={disabled ? undefined : onClick}
+      density='compact'
+    />
+  );
+}
