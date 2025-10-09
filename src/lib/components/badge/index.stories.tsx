ed8f083f526ebc2ec672a9fa7a613494094bd6b4
Diogo Mateus <diogo@feather-insurance.com>
2025-10-09 13:55:06 +0100

    refactor(badge): [PRO-4442] Update badge component (#470)

--- src/lib/components/badge/index.stories.tsx
+++ src/lib/components/badge/index.stories.tsx
@@ -18,12 +18,16 @@ const story = {
     variant: {
       description: 'Variant that defines the style of the Badge',
     },
+    showDot: {
+      description: 'Whether to show a dot indicator',
+    }
   },
   args: {
     children: 'Value',
     className: '',
     size: 'medium',
     variant: 'information',
+    showDot: false
   }
 };
  
@@ -32,14 +36,75 @@ export const BadgeStory = ({
   className,
   size,
   variant,
+  showDot,
 }: BadgeProps) => (
   <Badge
     className={className}
     size={size}
     variant={variant}
+    showDot={showDot}
   >
     {children}
   </Badge>
+);
+
+BadgeStory.storyName = "Badge";
+
+const variants: BadgeProps['variant'][] = [
+  'white',
+  'neutral',
+  'neutralStrong',
+  'information',
+  'warning',
+  'primary',
+  'error',
+  'success',
+  'secondary',
+  'secondaryStrong',
+  'black',
+];
+
+export const Variants = () => (
+  <div className='d-flex fd-column gap16'>
+    {variants.map((variant) => (
+      <div key={variant} className='d-flex'>
+        <div className='ws3'>
+          <Badge variant={variant}>
+            {variant}
+          </Badge>
+        </div>
+        <div className='ws3 ml32'>
+          <Badge
+            variant={variant}
+            showDot
+          >
+            {variant}
+          </Badge>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+const sizes: BadgeProps['size'][] = ['xsmall', 'small', 'medium', 'large'];
+
+export const Sizes = () => (
+  <div className='d-flex fd-column gap16'>
+    {sizes.map((size) => (
+      <div key={size} className='d-flex'>
+        <div className='ws3'>
+          <Badge size={size}>
+            {size}
+          </Badge>
+        </div>
+        <div className='ws3 ml32'>
+          <Badge size={size} showDot>
+            {size}
+          </Badge>
+        </div>
+      </div>
+    ))}
+  </div>
 );
 
 BadgeStory.storyName = "Badge";
--- src/lib/components/badge/index.tsx
+++ src/lib/components/badge/index.tsx
@@ -3,51 +3,55 @@ import classNames from 'classnames';
 import styles from './style.module.scss';
 
 type Variant = 
- | 'warning'
- | 'error'
- | 'success'
- | 'information'
- | 'neutral'
- | 'neutral200'
- | 'neutral300'
- | 'primary'
- | 'primary900';
+  | 'white'
+  | 'neutral'
+  | 'neutralStrong'
+  | 'information'
+  | 'warning'
+  | 'primary'
+  | 'error'
+  | 'success'
+  | 'black'
+  | 'secondary'
+  | 'secondaryStrong';
 
 export interface BadgeProps {
   className?: string;
   variant?: Variant;
-  size?: 'small' | 'medium' | 'large';
+  size?: 'xsmall' | 'small' | 'medium' | 'large';
   children: ReactNode;
+  showDot?: boolean;
 }
-
-const getVariantClassNames = (variant: Variant) => ({
-  information: 'bg-blue-100',
-  neutral: 'bg-white',
-  neutral200: 'bg-neutral-100',
-  neutral300: 'bg-neutral-300',
-  warning: 'bg-yellow-200',
-  error: 'bg-red-100',
-  success: 'bg-green-100',
-  primary: 'bg-purple-300',
-  primary900: 'bg-purple-900 tc-white',
-}[variant])
 
 const Badge = ({
   className = '',
   size = 'medium',
   variant = 'information',
   children,
+  showDot = false
 }: BadgeProps) => (
   <div
     role="status" 
     className={classNames(
-      className, 
-      'px16 br8 d-inline-block ai-center fw-bold p-p', 
-      { 'p-p--small': size === 'small' },
+      className,
+      'br8 d-inline-flex ai-center fw-bold p-p',
+      styles.badge,
       styles[`badge--${size}`],
-      getVariantClassNames(variant)
+      styles[`badge--${variant}`],
+      {
+        'p-p--small': size === 'small' || size === 'xsmall',
+      }
     )}
   >
+    {showDot && (
+      <span
+        className={classNames(
+          styles.badgeDot,
+          styles[`badgeDot--${variant}`]
+        )}
+      />
+    )}
+
     {children}
   </div>
 );
